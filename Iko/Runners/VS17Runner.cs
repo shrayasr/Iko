59b7ed5dd8bc4597fd3a03191bd9a682bc9268a5
SpartanX1 <abhi.secret@yahoo.com>
2018-04-22 12:05:59 +0530

    Get VS17 exe path from registry

--- Iko/Runners/VS17Runner.cs
+++ Iko/Runners/VS17Runner.cs
@@ -2,19 +2,47 @@
 using Nett;
 using System;
 using System.Diagnostics;
+using System.Globalization;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
 
 namespace Iko.Runners
 {
     public class VS17Runner : IRunner
     {
+        
         public void Run(TomlTable table)
         {
             var @params = new VS17Params(table);
 
-            Process.Start(
-                    "cmd",
-                  $@"/c start ""C:\Program Files(x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe"" {@params.Path}"
-                );
+            var path = GetVisualStudioInstallationFolder();
+
+            Process.Start("cmd", $@"/c start "+path+" {@params.Path}");
+        }
+
+        //Requires admin privileges 
+        private string GetVisualStudioInstallationFolder()
+        {
+            try
+            {
+                var vs17exe=string.Empty;
+                var vs17BaseDirectory = Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Microsoft\\VisualStudio\\SxS\\VS7", true);
+
+                if (vs17BaseDirectory.GetValue("15.0") != null)
+                {
+                    vs17exe = vs17BaseDirectory.GetValue("15.0") + @"Common7\IDE\devenv.exe";
+                    return vs17exe;
+                }
+                else
+                {
+                    throw new ArgumentException("Could not find the path for VS15. Registry value does not exist");
+                }
+                    
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.ToString());
+            }
         }
 
         class VS17Params
